9ab0502c7f178fef83975255d53f91023fafa61c
Dev Jangid <devjangid@users.noreply.github.com>
2026-02-07 22:27:46 +0530

    use render port

--- server/index.js
+++ server/index.js
@@ -22,6 +22,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT, () => {
+  console.log("server running on port", PORT);
 });
