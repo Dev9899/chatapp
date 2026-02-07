328316bcaedd3e40b8ca5adba51e7f2b648dcfa5
Dev Jangid <devjangid@users.noreply.github.com>
2026-02-07 23:50:47 +0530

    Added localstorage

--- client/script.js
+++ client/script.js
@@ -5,6 +5,32 @@ const messagesDiv = document.getElementById("messages");
 const messageInput = document.getElementById("message");
 const usernameInput = document.getElementById("username");
 const sendButton = document.getElementById("send");
+const saveUsernameBtn = document.getElementById('saveusernamebtn');
+const savedUsername =localStorage.getItem('chatappUsername')
+let username;
+
+if (saveUsernameBtn) {
+    username = saveUsernameBtn;
+    usernameInput.disabled = true;
+    document.querySelector('.modal').style.display = 'none';
+    document.querySelector('.modalback').style.display = 'none';
+
+    socket.emit('join', savedUsername);
+    hasJoined = true;
+} else {
+    saveUsernameBtn.addEventListener('click', () => {
+        username = usernameInput.value.trim();
+        if (!username || hasJoined) return;
+
+        localStorage.setItem('chatappUsername', username);
+        socket.emit('join', username)
+        hasJoined = true;
+        usernameInput.disabled = true;
+        document.querySelector('.modal').style.display = 'none';
+        document.querySelector('.modalback').style.display = 'none';
+        messageInput.focus();
+    })
+}
 
 // Receive messages
 socket.on("chat-message", (data) => {
@@ -30,7 +56,6 @@ socket.on("system-message", (data) => {
 // Send message
 sendButton.onclick = () => {
   const message = messageInput.value;
-  const username = usernameInput.value || "Anonymous";
 
   if (!hasJoined) {
     socket.emit("join", username);
